b15fd1de358202141cc207e3f576736d057c9a72
zahidhasanzh <zahidhasanzh000@gmail.com>
2024-12-30 08:16:06 +0600

    Add layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,20 @@
-
-import './App.css'
+import Layout from "./layouts/Layout"
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 
 function App() {
 
-
   return (
       <>
-        <h1 className='text-red-600'>hello</h1>
+        <Router>
+            <Routes>
+              <Route path="/" element={<Layout>
+                 <p>Home Page</p>
+              </Layout>}/>
+              <Route path="/search" element={<Layout>
+                  <p>Search Page</p>
+              </Layout>}/>
+            </Routes>
+        </Router>
       </>
   )
 }
