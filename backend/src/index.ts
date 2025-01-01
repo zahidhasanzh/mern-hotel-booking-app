2403964056cbfd7900cf97f7d2c309eab6f5f995
zahidhasanzh <zahidhasanzh000@gmail.com>
2025-01-01 19:30:51 +0600

    add hotel feature

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config({ path: process.env.DOTENV_CONFIG_PATH || '.env' }); 
@@ -7,6 +7,14 @@ import userRoutes from "./routes/users"
 import authRoutes from "./routes/auth"
 import cookieParser from "cookie-parser"
 import path from "path"
+import { v2 as cloudinary} from "cloudinary"
+import myHotelRoutes from "./routes/my-hotels"
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
 
 const app = express();
@@ -24,6 +32,11 @@ app.use(express.static(path.join(__dirname, "../../frontend/dist")))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
+app.use("/api/my-hotels", myHotelRoutes)
+
+app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"))
+})
 
 
 
